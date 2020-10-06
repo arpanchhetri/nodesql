a294892dfecda2866c65ab85621e6c60271ac87e
Greg <greg@entrostat.com>
2020-10-06 22:12:45 +0200

    Updated Logger functionality

--- api/src/loaders/logger/index.js
+++ api/src/loaders/logger/index.js
@@ -1,37 +1,62 @@
+// Require
+// =============================================================================
 const winston = require('winston');
+const moment = require('moment');
 
 class Logger {
-  constructor(fileName) {
-    this.fileName = fileName;
+  // Configuration
+  // =============================================================================
+  constructor(functionName) {
+    this.functionName = functionName;
+    this.timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+
     this.logger = winston.createLogger({
+      format: winston.format.json(),
       transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({
-          filename: `./logs/${fileName}.log`
-        })
-      ],
-      format: winston.format.printf((info) => {
-        let message = `[${info.level.toUpperCase()}] [Filename: ${fileName}.log] - ${info.message}`
-        message = info.obj ? message + `data:${JSON.stringify(info.obj)} | ` : message
-        return message
-      })
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.printf(info => {
+              let message = `${this.timestamp} - [${info.level.toUpperCase()}] [functionName: ${this.functionName}]: ${info.message}`;
+              if(info.details) {
+                message = message + `=> ${info.details}`;
+              }
+              return message;
+            })
+          )
+        }),
+        new winston.transports.File({filename: './logs/logs.log'}),
+        new winston.transports.File({filename: './logs/errors.log', level: 'error'})
+      ]
     });
   }
 
-  info(message) {
-    this.logger.info(message);
+  info(message, details) {
+    this.logger.info({
+      message: message,
+      details: details
+    });
   }
 
-  debug(message) {
-    this.logger.debug(message);
+  debug(message, details) {
+    this.logger.debug({
+      message: message,
+      details: details
+    });
   }
 
-  error(message) {
-    this.logger.error(message);
+  error(message, details) {
+    this.logger.error({
+      message: message,
+      details: details
+    });
   }
 
-  warn(message) {
-    this.logger.warn(message);
+  warn(message, details) {
+    this.logger.warn({
+      message: message,
+      details: details
+    });
   }
 }
 
