e6cb937c38cdab4dde19a86e71e444a08e0c9780
Greg <greg@entrostat.com>
2020-10-06 22:19:17 +0200

    Updated Loaders Index file

--- api/src/loaders/index.js
+++ api/src/loaders/index.js
@@ -1,5 +1,11 @@
 /**
  * LOADERS
  * This should be used to split the startup process into modules.
- * 
- *  *  */ 
+ *
+ *  *  */
+
+const Logger = require('../modules/logger/index');
+
+module.exports = {
+  Logger: Logger
+}
